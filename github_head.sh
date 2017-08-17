1324822069676aafebc925608f1e3bf31b8697ab
Virgil Dupras <hsoft@hardcoded.net>
2017-08-16 20:35:37 -0400

    Add support for versioned dependencies

--- github_head.sh
+++ github_head.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 EGO_PN=$1
+GIT_BRANCH=${2:-HEAD}
 
-curl -sS https://api.github.com/repos/${EGO_PN#github.com/}/commits/HEAD | jq -r .sha
+curl -sS "https://api.github.com/repos/${EGO_PN#github.com/}/commits/${GIT_BRANCH}" | jq -r .sha
--- update_ego_vendor.sh
+++ update_ego_vendor.sh
@@ -12,7 +12,9 @@ done
 sed -n -r 's#\s*"(.+) [0-9a-f]+ github.com/(.+)"#\1 \2#p' "$EBUILD_PATH" | while read -r line
 do
     words=( $line )
-    printf "\t\"%s %s github.com/%s\"\n" "${words[0]}" "$( ./github_head.sh "${words[1]}" )" "${words[1]}"
+    git_branch="$(expr "${words[0]}" : '.*\.\(v[0-9]\)')"
+    commit_hash=$( ./github_head.sh "${words[1]}" "${git_branch}" )
+    printf "\t\"%s %s github.com/%s\"\n" "${words[0]}" "${commit_hash}" "${words[1]}"
 done
 
 echo ")"
