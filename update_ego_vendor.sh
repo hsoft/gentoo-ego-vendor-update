af1dcee625afd4f25890b90e6d6e8351a0896097
Virgil Dupras <hsoft@hardcoded.net>
2017-08-16 20:04:11 -0400

    Fix printing of github ids

--- update_ego_vendor.sh
+++ update_ego_vendor.sh
@@ -6,13 +6,13 @@ echo "EGO_VENDOR=("
 
 sed -n -r 's#\s*"github.com/(.+) [0-9a-f]+"#\1#p' "$EBUILD_PATH" | while read -r line
 do
-    printf "\t\"%s %s\"\n" "${line}" "$( ./github_head.sh "${line}" )"
+    printf "\t\"github.com/%s %s\"\n" "${line}" "$( ./github_head.sh "${line}" )"
 done
 
 sed -n -r 's#\s*"(.+) [0-9a-f]+ github.com/(.+)"#\1 \2#p' "$EBUILD_PATH" | while read -r line
 do
     words=( $line )
-    printf "\t\"%s %s %s\"\n" "${words[0]}" "$( ./github_head.sh "${words[1]}" )" "${words[1]}"
+    printf "\t\"%s %s github.com/%s\"\n" "${words[0]}" "$( ./github_head.sh "${words[1]}" )" "${words[1]}"
 done
 
 echo ")"
