3121a0b13f523558a2d12cc7831ea4391953321f
Virgil Dupras <hsoft@hardcoded.net>
2017-10-29 19:49:13 -0400

    Use "branch" comments instead of implicit branch selection
    It seems that the "v1" "v2" etc. branching convention isn't applied in
    every go package so we need explicit branch fetching selection (when not
    `HEAD`)

--- update_ego_vendor.sh
+++ update_ego_vendor.sh
@@ -4,17 +4,21 @@ EBUILD_PATH=$1
 
 echo "EGO_VENDOR=("
 
-sed -n -r 's#\s*"github.com/(.+) [0-9a-f]+"#\1#p' "$EBUILD_PATH" | while read -r line
+sed -n -r 's!\s*"github.com/(.+) [0-9a-f]+"( # branch )?(\w+)?!\1 \3!p' "$EBUILD_PATH" | while read -r line
 do
+    words=( $line )
     printf "\t\"github.com/%s %s\"\n" "${line}" "$( ./github_head.sh "${line}" )"
 done
 
-sed -n -r 's#\s*"(.+) [0-9a-f]+ github.com/(.+)"#\1 \2#p' "$EBUILD_PATH" | while read -r line
+sed -n -r 's!\s*"(.+) [0-9a-f]+ github.com/(.+)"( # branch )?(\w+)?!\1 \2 \4!p' "$EBUILD_PATH" | while read -r line
 do
     words=( $line )
-    git_branch="$(expr "${words[0]}" : '.*\.\(v[0-9]\)')"
+    git_branch=${words[2]}
     commit_hash=$( ./github_head.sh "${words[1]}" "${git_branch}" )
-    printf "\t\"%s %s github.com/%s\"\n" "${words[0]}" "${commit_hash}" "${words[1]}"
+    if [ ! -z "$git_branch" ]; then
+        line_suffix=" # branch ${git_branch}"
+    fi
+    printf "\t\"%s %s github.com/%s\"%s\n" "${words[0]}" "${commit_hash}" "${words[1]}" "${line_suffix}"
 done
 
 echo ")"
